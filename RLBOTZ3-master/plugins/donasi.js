99418c319a9fc375c2b647262020f691c8ca4d59
Ramajs <ramadhan555666@gmial.com>
2022-12-11 12:57:37 +0700

    bebas

---
+++ RLBOTZ3-master/plugins/donasi.js
@@ -0,0 +1,27 @@
+let handler = async m => m.reply(`
+「 Suport Bot Dengan Cara donasi 」
+
+⫹⫺ Via Pulsa Telkomsel : ------
+⫹⫺ Via Dana : 6285806240904
+⫹⫺ Via Saweria : -----
+⫹⫺ Via Sopepey : 6285806240904
+
+*Note:* Hasil Donasi Nantinya Akan Digunakan Untuk Membayar Web Host Agar Bot Dapat Online 24 Jam Nonstop.
+
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+handler.rowner = false
+handler.mods = false
+handler.premium = true
+handler.group = false
+handler.private = true
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = false
+
+export default handler 
